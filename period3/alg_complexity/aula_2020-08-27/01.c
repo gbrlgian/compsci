c8985e9a571319020019a36ad74d53d5051b0a4b
Gabriel Gian <gabrielgian@protonmail.com>
2020-08-27 21:12:36 -0300

    Add class

---
+++ period3/alg_complexity/aula_2020-08-27/01.c
@@ -0,0 +1,9 @@
+selection_sort(list[], tam)
+{
+    int i, j, min, aux;
+    for (i = 0; i < (len(list) - 1); i++) 
+    {
+        min - 1;
+        for (j = (i + 1); j < (len(list)); j++)
+        {
+
