9ef9a1619e263479702a489619bf46041514488c
Gabriel Gian <gabrielgian@protonmail.com>
2020-11-12 21:13:23 -0300

    Update algorithms portfolio

--- period4/alg_complexity/portfolio.py
+++ period4/alg_complexity/portfolio.py
@@ -1,81 +1,107 @@
-#Bem errado
+import json
 
-def registrar():
-	nome = input("Informe o nome do jogo: ")
-	preco = input("Informe o preço do jogo: ")
-	banco = {'nome':preco}
-	return banco
+with open('games.json', 'r') as file:
+	games_dic = json.load(file)
 
-def ordenarNome(dic, alg):
+
+def printDic(dic):
+
+	for x in dic:
+		print(list(dic.keys()))
+
+
+def toList(dic, val):
+
+	if not dic and val:
+		return []
+
 	lst = []
-	lst2 = []
-	ordDic = {} 
-	lst = dic.items()
 
-	if alg == "quick":
-		lst2 = quicksort(lst, False)
-		ordDic = lst2
-		return ordDic
+	if val.upper() == "NOME":
+    
+		for key in list(dic.keys()):
+			lst.append([key])
 
-	elif alg == "bubble":
-		lst2 = bubblesort(lst, False)
-		ordDic = lst2
-		return ordDic
-
-	else: 
-		return []
-
-def ordenarPreco(dic, alg):
-	lst= []
-	lst2 = []
-	ordDic = {} 
-	lst = dic.items()
-
-	if alg == "quick":
-		lst2 = quicksort(lst, True)
-		ordDic = lst2
-		return ordDic
-
-	elif alg == "bubble":
-		lst2 = bubblesort(lst, True)
-		ordDic = lst2
-		return ordDic
-
-	else: 
-		return []
-
-def quicksort(lista, num):
-	if not lista:
-			return []
-
-	if num == True:
-		
-		return quicksort([(x[1] for x in lista[0:]
-		if x[1:] < x[0]) + (x[1] for x in lista[0]) +
-		(x[1] for x in lista[0:]
-		if x[1:] >= x[0])], True)
-
-	else:
-		
-		return quicksort([(x[0] for x in lista[0:]
-		if x[1:] < x[0]) + (x[0] for x in lista[0]) +
-		(x[0] for x in lista[0:]
-		if x[1:] >= x[0])], False)
-
-def bubblesort(lista, num):
-	if not lista:
-		return []
-
-	if num == True:
-		
-		return(bubblesort([x[1] for x in lista[0:] 
-		if x[1:] > lista[0:]], True))
+	if val.upper() == "VALOR":
+    
+		for val in list(dic.values()):
+    		lst.append([val])
 	
-	else:
-	
-		return(bubblesort([x[1] for x in lista[0:] 
-		if x[1:] > lista[0:]], False))
+	return lst
 
 
+def partitionQuick(list, begin, end):
+    pivot = list[begin]
+    i = begin + 1
+    j = end - 1
+ 
+    while True:
+        while (i <= j and list[i] <= pivot):
+            i += 1
 
-ordenarNome(registrar(), "quick")
+        while (i <= j and list[j] >= pivot):
+            j -= 1
+ 
+        if i <= j:
+           list[i], list[j] = list[j], list[i]
+
+        else:
+            list[begin], list[j] = list[j], list[begin]
+            return j
+
+
+def quickSort(list, begin, end):
+
+	if not lista:
+    	print("Lista vazia.")
+    	return []
+  
+	if end - begin > 1:
+		p = partitionQuick(list, begin, end)
+		quickSort(list, begin, p)
+		quickSort(list, p + 1, end)
+		
+
+def merge(left_half, right_half):
+
+    result_lst = []
+
+    while len(left_half) != 0 and len(right_half) != 0:
+        if left_half[0] < right_half[0]:
+            result_lst.append(left_half[0])
+            left_half.remove(left_half[0])
+
+        else:
+            result_lst.append(right_half[0])
+            right_half.remove(right_half[0])
+
+    result_lst += right_half if len(left_half) == 0 else left_half
+
+    return result_lst
+
+
+def mergeSort(uns_list):
+
+    if len(uns_list) <= 1:
+        return uns_list
+    
+    middle = len(uns_list) // 2
+
+    left_list = uns_list[:middle]
+    right_list = uns_list[middle:]
+    left_list = mergeSort(left_list)
+    right_list = mergeSort(right_list)
+
+    return list(merge(left_list, right_list))
+
+
+printDic(games_dic)
+
+def sorter(dic)
+
+	val = input("Digite \"NOME\" para ordenar as entradas do dicionário por nome; \nOu, então, digite \"VALOR\" para ordernar as entradas por valor")
+	games_lst = []
+	games_lst = toList(games_dic, val)
+
+	alg = input("Digite \"QUICK\" para ordenar o dicionário usando o algoritmo Quick Sort; \nOu, então, digite \"MERGE\" para ordernar o dicionário usando o Merge Sort.")
+quickSort(games_lst, 0, len(games_lst))
